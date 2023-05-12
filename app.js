bd1877907b1b83dc3061379d5376c1adab7a5fd1
Zach Kirsch <zachkirsch@gmail.com>
2023-05-12 13:18:14 -0700

    Upgrade to latest version of SDK

---
+++ app.js
@@ -0,0 +1,18 @@
+import { BelvoClient, Belvo, BelvoEnvironment } from '@fern-api/belvo';
+void main();
+async function main() {
+    const belvo = new BelvoClient({
+        secretId: 'YOUR_SECRET_ID',
+        secretPassword: 'YOUR_SECRET_PASSWORD',
+        environment: BelvoEnvironment.Sandbox,
+    });
+    const link = await belvo.links.registerLink({
+        body: {
+            institution: 'banamex_mx_retail',
+            username: 'username',
+            password: 'password',
+            accessMode: Belvo.EnumLinkAccessModeRequest.Single,
+            credentialsStorage: '30d',
+        },
+    });
+}
